681be7069e42517e2621d287bfcce95897dc5076
LeonelMadrid13 <leoneleduardomadrid@hotmail.com>
2025-07-18 10:23:05 -0600

    Refactor fetchUserActivity function for improved error handling and clarity

--- GitHub-User-Activity/github-activity.js
+++ GitHub-User-Activity/github-activity.js
@@ -6,27 +6,41 @@ if (!username) {
   process.exit(1);
 }
 
-const url = `https://api.github.com/users/${username}/events/public`;
 
-const response = await fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`GitHub API error: ${response.statusText}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    const activities = data.map(event => {
-      return {
-        type: event.type,
-        repo: event.repo.name,
-        created_at: event.created_at
-      };
-    });
-    return JSON.stringify(activities, null, 2);
-  })
-  .catch(error => {
+async function fetchUserActivity(username) {
+  try {
+    const url = `https://api.github.com/users/${username}/events/public`;
+    const response = await fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`GitHub API error: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const activities = data.map(event => {
+          return {
+            type: event.type,
+            repo: event.repo.name,
+            created_at: event.created_at
+          };
+        });
+        return JSON.stringify(activities, null, 2);
+      });
+      return response;
+  } catch (error) {
     console.error('Error fetching GitHub user activity:', error);
-  });
+  }
+}
 
-  console.log(response)
+/**
+ *  Output:
+ *  - Pushed 3 commits to kamranahmedse/developer-roadmap
+ *  - Opened a new issue in kamranahmedse/developer-roadmap
+ *  - Starred kamranahmedse/developer-roadmap
+ *  - ...
+ */
+
+const response = await fetchUserActivity(username);
+console.log(response);
+process.exit(0);
